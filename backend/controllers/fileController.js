b63597328f852aafffa3207f0a8d06c3d725919e
Swezal Manhas <swezalmanhas@gmail.com>
2024-11-23 14:58:32 +0530

    uploads

--- backend/controllers/fileController.js
+++ backend/controllers/fileController.js
@@ -9,7 +9,13 @@ const libre = require("libreoffice-convert");
 
 // Multer setup for file uploads
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => cb(null, "uploads/"),
+  destination: (req, file, cb) => {
+    const uploadDir = path.join(__dirname, "../uploads");
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+    cb(null, uploadDir);
+  },
   filename: (req, file, cb) => cb(null, `${Date.now()}-${file.originalname}`),
 });
 const upload = multer({ storage });

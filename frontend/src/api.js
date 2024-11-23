dac4139949ce84ea8d0abafc422bc4a08aeb9a0c
Swezal Manhas <swezalmanhas@gmail.com>
2024-11-23 15:18:46 +0530

    docker

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://word-pdf-1.onrender.com",
+  baseURL: "https://word-to-pdf-assignment.onrender.com",
 });
 
 export default instance;
